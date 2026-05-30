9bd777fc63b5a4c2c16742de045f76aa969597fe
Justin Sweers <justsweers@comcast.net>
2026-05-30 06:26:28 -0700

    Started (VALIDATE) subroutine with the first check against spaces. Added some comments to (INPUT) to make things more clear.

--- FreqDist.asm
+++ FreqDist.asm
@@ -43,14 +43,14 @@ INPUT
 	ADD R1, R1, #1
 	OUT
 	
-INLOOP
+INLOOP									; Input subroutine "loop" label.
 	; Test if string has hit upper bound.
 	LD R4, IN_LEN
 	NOT R4, R4							; Two's compliment to negative.
 	ADD R4, R4, #1
 	ADD R1, R1, R4
 	BRz INDONE							; Input buffer filled, must return.
-	NOT R4, R4							; Two's compliment back to positive.
+	NOT R4, R4							; Two's compliment back to positive. 
 	ADD R4, R4, #1
 	ADD R1, R1, R4
 	; Test if character in R0 is Newline.
@@ -75,8 +75,10 @@ INLOOP
 	OUT
 	ADD R1, R1, #1
 	BR INLOOP
+	
+	; TODO: Optimize 2nd Two's Compliments statements to LD instructions instead.
 
-INDONE
+INDONE									; Input subroutine "Done" label. Used to escape from INLOOP.
 	; Load registers R0-R7 to the stack, excluding R6 since it is constant.
 	LTR R0, R6, #0
 	LTR R1, R6, #1
@@ -96,7 +98,62 @@ INVALID
 	LEA R0, ERR
 	PUTS
 	HALT
+
+
+; =========================
+; Validate Subroutine
+; Validates input and counts letter frequency
+; =========================
+VALIDATE
+; Save registers R0-R7 to the stack, excluding R6 since it is constant.
+STR R0, R6, #0
+	STR R1, R6, #1
+	STR R2, R6, #2
+	STR R3, R6, #3
+	STR R4, R6, #4
+	STR R5, R6, #5
+	STR R7, R6, #7
+
+	; Init values — 
+	; R0 is the current character PTR,
+	; R1 is iterative counter,
+	; R2 is compartive (==, >=, etc.) operand,
+	; R3 is current character VALUE.
+	LEA R0, INP
+	AND R1, R1, #0
+	AND R2, R2, #0
+	LDI R3, R0
+		
+VALLOOP									; Validate subroutine "Loop" label.
+	; Test if (current character value) == (ASCII Space)
+	LD R2, ASC_SP
+	NOT R2, R2							; Two's compliment to negative.
+	ADD R2, R2, #1
+	ADD R3, R2, R3
+	BRz VALCONT							; Current character is (ASCII Space), initiate C-style "continue".
+	LD R2, ASC_SP						; Restore back to positive.
 	
+	
+VALCONT									; Validate subroutine "Continue" label. C-style "for-loop" processes handled here. Branched from various places inside VALLOOP.
+	; Increment (counter), set new (current character) ptr & value
+	ADD R1, R1, #1						; Increment counter
+	LEA R0, INP							; Set new current character ptr
+	ADD R0, R0, R1
+	LDI R3, R0							; Set new character value
+	; Test loop condition
+	BRz VALEXIT							; Current character is 0x00, which is not a valid keyboard character, end loop.
+	BR VALLOOP
+	
+	
+VALEXIT
+	LTR R0, R6, #0
+	LTR R1, R6, #1
+	LTR R2, R6, #2
+	LTR R3, R6, #3
+	LTR R4, R6, #4
+	LTR R5, R6, #5
+	LTR R7, R6, #7
+	RET
 	
 ; Data
 
---
+++ README.md
@@ -0,0 +1,18 @@
+## Abstract
+
+This project is for the team project of 26SPR-CIS-11-22428 at Moreno Valley College, lead by Kasey Nguyen.
+It contains LC-3 assembly source code for the end-of-semester project, of which Option C: "Character Counter for Names" was selected.
+Keanu Arao, Justin Sweers, and Suleny Perez are authors of this code.
+
+
+
+## Input/Output
+
+The input allows for 100 characters, of which, the letters A-Z are allowed of either capitalization, plus the space character.
+The output is a [Frequency Distribution](https://www.geeksforgeeks.org/maths/frequency-distribution/) of the letters from the input, along with their according number of appearances, newline delimited, and in alphabetical order.
+
+
+
+## Usage Instructions
+
+Only the source code is provided, you must assemble from source yourself using LC-3's assembler. Additionally, you must use LC-3's official simulator to run the program. Both programs for doing this can be found [here](https://highered.mheducation.com/sites/0072467509/student_view0/lc-3_simulator.html).
